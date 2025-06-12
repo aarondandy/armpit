ddcacbe58daa522ddcb72b4dbd068c5919917df1
user <user@localhost.com>
2025-06-11 21:16:04 -0700

    Expand name hash encodings

--- lib/src/nameHash.test.ts
+++ lib/src/nameHash.test.ts
@@ -8,7 +8,7 @@ expect.extend({
     const expectedPrefixValue = expectedPrefix.toString();
     return {
       pass: receivedValue.startsWith(expectedPrefixValue),
-      message: () => `Expected '${receivedValue}' to${isNot ? " not" : ""} start with ${expectedPrefixValue}`
+      message: () => `Expected '${receivedValue}' ${isNot ? "to not" : "to"} start with ${expectedPrefixValue}`
     };
   }
 })
@@ -22,23 +22,86 @@ declare module "vitest" {
 }
 
 describe("alphanumeric generation long-term stability by size", () => {
-  const givenName = "abc";
-  const expectedHugeHashOfAbcAlphaNum = "y1rm3to8s85ef3n6dlmklfa8xxi6uz1nh60incdk2n3ogph111cgnp06yuixctglj3it7bxgytoo9sdie630pabs50egbrj3gz1i4u8k2nm7wt48kunng6xqee18gu9sxyhlzpnkxvktiegtymumpqm08za6md9khb7n3bju5kb8ca15uulih15zdspg9pyidqug94tw";
+  const givenName = "abc123-input";
+  const longTermHugeHash = "52gyylw97shv2josded10rtx6xpcf43u694vs1t5tdo9xtgm2eggxvu5k0jzzufj5zmkcen3sitpnxzxt9pkup4fvg4un409k30ysuvpzi76a1ns718qv6maxz63wwp9imnl0mh3ax2sm4eimf23af0f8qsean5oyr8j0ddqa1lapmj0dquqvwnn8k6gkif45hr27wdb";
 
   it("small", () => {
     const actual = new NameHash(givenName, { type: "alphanumeric", defaultLength: 2 })();
     expect(actual).toHaveLength(2);
-    expect(expectedHugeHashOfAbcAlphaNum).toStartWith(actual);
+    expect(longTermHugeHash).toStartWith(actual);
   });
   it("default", () => {
     const actual = new NameHash(givenName, { type: "alphanumeric", defaultLength: undefined })();
-    expect(actual).toHaveLength(4);
-    expect(expectedHugeHashOfAbcAlphaNum).toStartWith(actual);
+    expect(actual).toHaveLength(3);
+    expect(longTermHugeHash).toStartWith(actual);
   });
   it("huge", () => {
     const actual = new NameHash(givenName, { type: "alphanumeric", defaultLength: 200 })();
     expect(actual).toHaveLength(200);
-    expect(expectedHugeHashOfAbcAlphaNum).toStartWith(actual);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+});
+
+describe("alpha generation long-term stability by size", () => {
+  const givenName = "abc-input";
+  const longTermHugeHash = "zfkfshrxmzmukpmblrujwrqlgyuapazvaqmkqpwwbmasucylqgsxculgduxoeyoxhurxoawbrayfxmrowhtlnkvkjkrfavlvakozmvbnycldislqkqhajiwcnrtuxujgkvmephsaclttyircvilimajspfjweyxtldahrpqlyeyknnjltgnuxjsivbniisnxcgzzjyqi";
+
+  it("small", () => {
+    const actual = new NameHash(givenName, { type: "alpha", defaultLength: 2 })();
+    expect(actual).toHaveLength(2);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("default", () => {
+    const actual = new NameHash(givenName, { type: "alpha", defaultLength: undefined })();
+    expect(actual).toHaveLength(3);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("huge", () => {
+    const actual = new NameHash(givenName, { type: "alpha", defaultLength: 200 })();
+    expect(actual).toHaveLength(200);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+});
+
+describe("hex generation long-term stability by size", () => {
+  const givenName = "hex-input";
+  const longTermHugeHash = "29da8b0df30fd87061c535c46a15d6d928cf41cf6c7b3981ff2effbf183e04e202b08674ea7bc4f79a7ec281305121f10148fc7d1345b99d712bb81657d7581853f61b12c576fae511baedff632a8098e9f5138b8bd11a2f9e631cc0f66135032329edb1";
+
+  it("small", () => {
+    const actual = new NameHash(givenName, { type: "hex", defaultLength: 2 })();
+    expect(actual).toHaveLength(2);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("default", () => {
+    const actual = new NameHash(givenName, { type: "hex", defaultLength: undefined })();
+    expect(actual).toHaveLength(4);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("huge", () => {
+    const actual = new NameHash(givenName, { type: "hex", defaultLength: 200 })();
+    expect(actual).toHaveLength(200);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+});
+
+describe("numeric generation long-term stability by size", () => {
+  const givenName = "123-input";
+  const longTermHugeHash = "08877159949384226962477697075934455023491100904365852208127779627777045555170806496813727047157844761875393773458862297878925806559980622939143010804526444691686898114376839805663855593778981592919269";
+
+  it("small", () => {
+    const actual = new NameHash(givenName, { type: "numeric", defaultLength: 2 })();
+    expect(actual).toHaveLength(2);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("default", () => {
+    const actual = new NameHash(givenName, { type: "numeric", defaultLength: undefined })();
+    expect(actual).toHaveLength(4);
+    expect(longTermHugeHash).toStartWith(actual);
+  });
+  it("huge", () => {
+    const actual = new NameHash(givenName, { type: "numeric", defaultLength: 200 })();
+    expect(actual).toHaveLength(200);
+    expect(longTermHugeHash).toStartWith(actual);
   });
 });
 
@@ -70,9 +133,6 @@ describe("alphanumeric generation short-term stability", () => {
 });
 
 describe("alphanumeric input concatenation stability", () => {
-  const nameParts = ["abc", "def"];
-  const nameFull = "abcdef";
-
   it("hash concat matches input string concat", () => {
     // A script may be refactored in a way that it used to produce a single hasher
     // combining subscription and group name. After the introduction of a new group
@@ -112,32 +172,33 @@ describe("alphanumeric input concatenation stability", () => {
 });
 
 describe("stringification", () => {
+  const expected = `123${new NameHash("abc").toString()}456`;
   it("interpolated string", () => {
     const hash = new NameHash("abc");
     const actual = `123${hash}456`;
-    expect(actual).toBe("123y1rm456");
+    expect(actual).toBe(expected);
   });
   it("string concat", () => {
     const hash = new NameHash("abc");
     const actual = "123".concat(hash.toString(), "456");
-    expect(actual).toBe("123y1rm456");
+    expect(actual).toBe(expected);
   });
   it("string plus operator", () => {
     const hash = new NameHash("abc");
     const actual = "123" + hash + "456";
-    expect(actual).toBe("123y1rm456");
+    expect(actual).toBe(expected);
   });
   it("string plus operator hash lvalue", () => {
     const hash = new NameHash("abc");
     let actual = hash + "456";
     actual = "123" + actual;
-    expect(actual).toBe("123y1rm456");
+    expect(actual).toBe(expected);
   });
   it("string append operator", () => {
     const hash = new NameHash("abc");
     let actual = "123";
     actual += hash;
     actual += "456";
-    expect(actual).toBe("123y1rm456");
+    expect(actual).toBe(expected);
   });
 });
